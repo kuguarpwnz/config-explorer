a5c857cc779583b0f71ef4e8fc9292fbe3d32bde
Vladislav Baranchuk <kgrpwnz@gmail.com>
2017-04-11 10:47:02 +0300

    Add click handling

--- src/preview/index.js
+++ src/preview/index.js
@@ -1,4 +1,12 @@
 import ConfigExplorer from '../formatter';
 import state from './state';
 
-ConfigExplorer.create(document.getElementById('placeholder')).format(state);
+ConfigExplorer.create(document.getElementById('placeholder'))
+  .onNodeClick((e) => {
+    const collapsible = e.currentTarget.nextElementSibling;
+    if (collapsible && collapsible.className.split(' ').includes('collapsible')) {
+      const { style, style: { display } } = collapsible;
+      style.display = display === 'none' ? 'inline' : 'none';
+    }
+  })
+  .format(state);
