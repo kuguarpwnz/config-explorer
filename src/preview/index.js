3f9218f28b749eb2ec1fec103db8413499d7ddf4
Vladislav Baranchuk <kgrpwnz@gmail.com>
2017-04-11 12:25:29 +0300

    Refactor

--- src/preview/index.js
+++ src/preview/index.js
@@ -1,12 +1,27 @@
 import ConfigExplorer from '../formatter';
 import state from './state';
 
+const toggleClass = (element, toggleClass) => {
+  const classes = element.className.split(' ');
+  const result = classes.filter((value) => value !== toggleClass);
+  if (result.length === classes.length) {
+    result.push(toggleClass);
+  }
+  element.className = result.join(' ');
+};
+
 ConfigExplorer.create(document.getElementById('placeholder'))
   .onNodeClick((e) => {
     const collapsible = e.currentTarget.nextElementSibling;
-    if (collapsible && collapsible.className.split(' ').includes('collapsible')) {
-      const { style, style: { display } } = collapsible;
-      style.display = display === 'none' ? 'inline' : 'none';
+    if (collapsible) {
+
+      const classNames = collapsible.className.split(' ');
+      if (classNames.includes('_c-e-collapsible')) {
+        const { style, style: { display } } = collapsible;
+        style.display = display === 'none' ? 'inline' : 'none';
+        toggleClass(e.currentTarget, '_c-e-opened');
+        toggleClass(e.currentTarget, '_c-e-closed');
+      }
     }
   })
-  .format(state);
+  .print(state);
---
+++ src/utils/index.js
@@ -0,0 +1,11 @@
+const _is = (value, type) => typeof value === type;
+
+export const isObjectLike = (value) => value !== null && _is(value, 'object');
+
+export const isArray = (value) => ({}).toString.call(value) === '[object Array]';
+
+export const isString = (value) => _is(value, 'string');
+
+export const isFunction = (value) => _is(value, 'function');
+
+export const isObject = (value) => isObjectLike(value) && !isArray(value);
