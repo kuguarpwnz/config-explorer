1da0c2ca46c91c51e166e827f2d6f24f45074d47
Vladislav Baranchuk <kgrpwnz@gmail.com>
2017-04-11 09:02:59 +0300

    Test

--- src/formatter/index.js
+++ src/formatter/index.js
@@ -1,51 +1,79 @@
-export default class Formatter {
-  constructor(placeholder, state = {}, spaces = 2) {
+export default class {
+  constructor(placeholder, indent = 2) {
     this.placeholder = placeholder;
-    this.state = state;
-    this.spaces = spaces;
+    this.indent = indent;
+    this._classNamePrefix = '_config-explorer-';
   }
 
-  format() {
-    this.placeholder.innerHTML = `<pre>${this.constructor._generate(this.state)}</pre>`;
+  static create(...args) {
+    return new this(...args);
   }
 
-  static _generate(value, depth = 1) {
-    return this._isObjectLike(value) ?
+  format(state) {
+    this.placeholder.innerHTML = `<pre>${this._generate(state)}</pre>`;
+    return this;
+  }
+
+  _generate(value, depth = 1) {
+    return this.constructor._isObjectLike(value) ?
       this._generateObjectLike(value, depth) :
       this._generatePrimitive(value);
   }
 
-  static _generateObjectLike(objectValue, depth) {
-    return this._withBraces(
-      objectValue,
-      Object.keys(objectValue).reduce((result, objectKey, i) => {
-        const indent = i ? '\n' : '';
-        const key = this._isArray(objectValue) ? '' : `${objectKey}: `;
-        const value = this._generate(objectValue[objectKey], depth + 1);
-        const spaces = this._spaces(depth);
-        const comma = Object.keys(objectValue).length - 1 === i ? '' : ',';
-        
-        return result = `${result}${indent}${spaces}${key}${value}${comma}`;
-      }, ''),
-      depth - 1
-    );
+  _generateObjectLike(object, depth) {
+    const [openBrace, closeBrace] = (this.constructor._isArray(object) ? '[]' : '{}').split('');
+    const keys = Object.keys(object);
+    const { length: keysLength } = keys;
+    const spaces = keysLength ? this._spaces(depth - 1) : '';
+    const newLine = keysLength ? '\n' : '';
+    const isArray = this.constructor._isArray(object);
+
+    const value = keys.reduce((result, objectKey, i) => {
+      const indent = i ? '\n' : '';
+      const key = isArray ? '' : `${this._withClass('key', objectKey)}: `;
+      const jsonValue = this._generate(object[objectKey], depth + 1);
+      const spaces = this._spaces(depth);
+      const comma = keysLength - 1 === i ? '' : ',';
+
+      return result = `${result}${indent}${spaces}${key}${jsonValue}${comma}`;
+    }, '');
+
+    const generated = keysLength ?
+      this._withClass(
+        'collapsible',
+        `${newLine}${value}${newLine}${spaces}`
+      ) : '';
+
+
+    return `${openBrace}${generated}${closeBrace}`;
   }
 
-  static _spaces(value = 0) {
-    value *= 2;
+  _withClass(className = '', value) {
+    return `<span class="${this._getClass(className)}">${value}</span>`;
+  }
+
+  _getClass(className) {
+    return this._classNamePrefix + className;
+  }
+
+  _spaces(count = 0) {
+    count *= this.indent;
     let result = "";
-    for (let i = 0; i < value; ++i) {
+    for (let i = 0; i < count; ++i) {
       result = `${result} `;
     }
     return result;
   }
 
-  static _generatePrimitive(value) {
-    return this._isString(value) ? `"${value}"` : `${value}`;
+  _generatePrimitive(value) {
+    return this._withClass(
+      this.constructor._getPrimitiveClass(value),
+      this.constructor._isString(value) ? `"${value}"` : value
+    );
   }
 
-  static _withBraces(initialValue, formatted, depth) {
-    return Array.isArray(initialValue) ? `[\n${formatted}\n${this._spaces(depth)}]` : `{\n${formatted}\n${this._spaces(depth)}}`;
+  static _getPrimitiveClass(value) {
+    return (typeof value).toLowerCase();
   }
 
   static _isObjectLike(value) {
@@ -60,7 +88,7 @@ export default class Formatter {
     return typeof value === 'string';
   }
 
-  static _isObject(value) {
+  _isObject(value) {
     return this._isObjectLike(value) && !this._isArray(value);
   }
 
